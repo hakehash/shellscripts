360e60d8395af6edcfba74a85586f7aa6eedcbcc
hakehash <b7dedc8797ec8ty@gmail.com>
2016-11-11 17:22:34 +0900

    bug fix

--- voicetext.sh
+++ voicetext.sh
@@ -4,37 +4,37 @@
 API_KEY=
 
 #Do not change below.
-case $# in
- 0)
-  if [ ! $API_KEY ]; then
-    echo 'Please register at https://cloud.voicetext.jp/webapi/api_keys/new'
-    echo 'and then write YOUR_API_KEY in this script file.' 
-  else
+if [ ! $API_KEY ]; then
+  echo 'Please register at https://cloud.voicetext.jp/webapi/api_keys/new'
+  echo 'and then write YOUR_API_KEY in this script file.' 
+else
+  case $# in
+   0)
     echo 'usage: '
     echo "$ `basename $0` text [speaker] [emotion] [emotion_level] [pitch] [speed] [volume]"
-  fi
- ;;
- 1)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=haruka" | play -
- ;;
- 2)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" | play -
- ;;
- 3)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" | play -
- ;;
- 4)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" | play -
- ;;
- 5)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" | play -
- ;;
- 6)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" -d "speed=$6" | play -
- ;;
- 7)
-  curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" -d "speed=$6" -d "volume=$7" | play -
- ;;
-esac
+   ;;
+   1)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=haruka" | play -
+   ;;
+   2)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" | play -
+   ;;
+   3)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" | play -
+   ;;
+   4)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" | play -
+   ;;
+   5)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" | play -
+   ;;
+   6)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" -d "speed=$6" | play -
+   ;;
+   7)
+    curl "https://api.voicetext.jp/v1/tts" -u "$API_KEY:" -d "text=$1" -d "speaker=$2" -d "emotion=$3" -d "emotion_level=$4" -d "pitch=$5" -d "speed=$6" -d "volume=$7" | play -
+   ;;
+  esac
+fi
 
 unset API_KEY
