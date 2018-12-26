4c5f9ff5ce160b0c634f0a020f6aa97c905974ed
hakehash <b7dedc8797ec8ty@gmail.com>
2018-12-26 16:13:25 +0900

    minor fix

--- dyn2roff.sh
+++ dyn2roff.sh
@@ -3,11 +3,12 @@
 echo ".ft C\n.nf"
 DYNFILE=$1
 NR_ES=`awk '/^\*ELEMENT_SHELL$/{print NR}' $DYNFILE`
+NR_EE=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 $NR_ES | grep -v $NR_ES`
 NR_ND=`awk '/^\*NODE$/{print NR}' $DYNFILE`
 NR_SS=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 $NR_ND | grep -v $NR_ND`
 cat $DYNFILE | \
-  sed `expr $NR_ES + 1`,`expr $NR_ND - 1`d | \
-  sed `expr $NR_ES + 3`,`expr $NR_ES + $NR_SS - $NR_ND`d | \
+  sed `expr $NR_ES + 1`,`expr $NR_EE - 1`d | \
+  sed `expr $NR_ND - \( $NR_EE - $NR_ES \) + 2`,`expr $NR_ES - $NR_EE + $NR_SS`d | \
   sed 's/^\*ELEMENT_SHELL$/&\n.DS C\n(omitted)\n.DE\n/' | \
   sed 's/^\*NODE$/&\n.DS C\n(omitted)\n.DE\n/'
 
