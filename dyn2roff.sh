ece754d7fe94417942014281752e630ab1f33cac
hakehash <b7dedc8797ec8ty@gmail.com>
2019-01-13 18:32:11 +0900

    minor fix

--- dyn2roff.sh
+++ dyn2roff.sh
@@ -6,8 +6,9 @@ NR_ES=`awk '/^\*ELEMENT_SHELL$/{print NR}' $DYNFILE`
 NR_EE=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 $NR_ES | grep -v $NR_ES`
 NR_ND=`awk '/^\*NODE$/{print NR}' $DYNFILE`
 NR_SS=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 $NR_ND | grep -v $NR_ND`
+OMITT=--------------------------------\>8--------------------------------
 cat $DYNFILE | \
   sed `expr $NR_ES + 1`,`expr $NR_EE - 1`d | \
   sed `expr $NR_ND - \( $NR_EE - $NR_ES \) + 2`,`expr $NR_ES - $NR_EE + $NR_SS`d | \
-  sed 's/^\*ELEMENT_SHELL$/&\n-------------------------------->8================================/' | \
-  sed 's/^\*NODE$/&\n-------------------------------->8================================/'
+  sed "s/^\*ELEMENT_SHELL$/&\n$OMITT/" | \
+  sed "s/^\*NODE$/&\n$OMITT/"
