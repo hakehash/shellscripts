93016b282c5b74907f438613eb726836c2c8b082
hakehash <b7dedc8797ec8ty@gmail.com>
2019-01-22 16:13:33 +0900

    no comment

--- audyn.sh
+++ audyn.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-PATHTOLSDYNA=/mnt/d/LSDYNA/programs/
+PATH_TO_LSDYNA=/mnt/d/LSDYNA/programs/
 NAME_OF_EXEC=ls-dyna_smp_s_R10.0_winx64_ifort131.exe
-DYNA_I=
-DYNA_O=
-$PATHTOLSDYNA$NAME_OF_EXEC
+PATH_TO_KEYFILE=/mnt/f
+KEYWORD_FILENAME=20181226.dyn
+
+for i in `seq 3 9`
+do
+  YmdHMS=`date +%Y%m%d%H%M%S`
+  DYNA_I=$PATH_TO_KEYFILE/$YmdHMS/$KEYWORD_FILENAME
+  DYNA_O=$PATH_TO_KEYFILE/$YmdHMS/d3hsp
+  echo $PATH_TO_LSDYNA$NAME_OF_EXEC I\=$DYNA_I O\=$DYNA_O
+done
 
