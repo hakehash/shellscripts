58ee91d8be6abcc98c5b30b8993a2bec8170f361
hakehash <b7dedc8797ec8ty@gmail.com>
2019-01-23 12:37:25 +0900

    T

--- audyn.sh
+++ audyn.sh
@@ -7,9 +7,9 @@ KEYWORD_FILENAME=`basename $1`
 
 for i in `seq 3 9`
 do
-  YmdHMS=`date +%Y%m%d%H%M%S%N`
-  DYNA_I=$PATH_TO_KEYFILE/$YmdHMS/$KEYWORD_FILENAME
-  DYNA_O=$PATH_TO_KEYFILE/$YmdHMS/d3hsp
+  YmdHMS=`date +%Y%m%dT%H%M%S`
+  DYNA_I=$PATH_TO_KEYFILE/${YmdHMS}_${i}mm/$KEYWORD_FILENAME
+  DYNA_O=$PATH_TO_KEYFILE/${YmdHMS}_${i}mm/d3hsp
   echo $PATH_TO_LSDYNA$NAME_OF_EXEC I\=$DYNA_I O\=$DYNA_O
 done
 
