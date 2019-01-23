acfb473b40e6a61004477c240ebb3a6133e916d3
hakehash <b7dedc8797ec8ty@gmail.com>
2019-01-23 11:52:08 +0900

    minor change

--- audyn.sh
+++ audyn.sh
@@ -2,12 +2,12 @@
 
 PATH_TO_LSDYNA=/mnt/d/LSDYNA/programs/
 NAME_OF_EXEC=ls-dyna_smp_s_R10.0_winx64_ifort131.exe
-PATH_TO_KEYFILE=/mnt/f
-KEYWORD_FILENAME=20181226.dyn
+PATH_TO_KEYFILE=`dirname $1`
+KEYWORD_FILENAME=`basename $1`
 
 for i in `seq 3 9`
 do
-  YmdHMS=`date +%Y%m%d%H%M%S`
+  YmdHMS=`date +%Y%m%d%H%M%S%N`
   DYNA_I=$PATH_TO_KEYFILE/$YmdHMS/$KEYWORD_FILENAME
   DYNA_O=$PATH_TO_KEYFILE/$YmdHMS/d3hsp
   echo $PATH_TO_LSDYNA$NAME_OF_EXEC I\=$DYNA_I O\=$DYNA_O
