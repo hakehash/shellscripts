3e6cc04d8a5333b6215203a61e741a8f38f7d7d2
hakehash <b7dedc8797ec8ty@gmail.com>
2019-09-05 17:36:58 +0900

    minor fix

--- secf2csv.sh
+++ secf2csv.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-ORIG=`cat $1 | grep line -n | tail -n1 | sed -e 's/:.*//g'`
-cat $1 | awk '{if((NR-'$ORIG')%4==1) print $2 "," $3}' > `echo $1.csv`
+if [ $# -eq 0 ]; then
+  echo "usage:\tfind ./path/to/secforc | xargs -L1 `basename $0`" 1>&2
+else
+  ORIG=`cat $1 | grep line -n | tail -n1 | sed 's/:.*//g'`
+  cat $1 | awk '{if((NR-'$ORIG')%4==1) print $2 "," $3}' > ${1}.csv
+fi
+
