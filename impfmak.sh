0cf04a0d01ea6d310cef57beeb26b3f4f818c177
hakehash <b7dedc8797ec8ty@gmail.com>
2019-09-10 18:50:32 +0900

    minor change

--- impfmak.sh
+++ impfmak.sh
@@ -7,8 +7,7 @@ LOCAL=0
 if [ $# -lt 3 ] || [ ! -f $1 ]; then
   echo "usage:\t`basename $0` file w0max -[ghl]" 1>&2
 else
-  cat $1 >.tmp_impfmak
-  DYNFILE=.tmp_impfmak
+  DYNFILE=$1
   w0max=$2
   while [ $# -gt 2 ]; do
     if [ $3 = "-g" ]; then
@@ -36,8 +35,15 @@ else
         else print$0
       }'
   fi
-fi
-if [ -w .tmp_dyn2roff ]; then
-  rm .tmp_dyn2roff
+
+  if [ $LOCAL -ne 0 ]; then
+    cat $DYNFILE | \
+      awk '{
+        if (NR > '$NR_NODE' && NR < '$NR_NEXT' && $4==0){
+          system("'$PATH_TO_SCRIPTS'/local.pl "sprintf("%d %f %f %f %f",$1,$2,$3,$4,'$w0max'))
+        }
+        else print$0
+      }'
+  fi
 fi
 
