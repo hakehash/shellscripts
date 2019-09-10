14ab8cb18f4a780d79348da72ba026f8e1e87374
hakehash <b7dedc8797ec8ty@gmail.com>
2019-09-10 17:51:38 +0900

    update

--- impfmak.sh
+++ impfmak.sh
@@ -1,14 +1,39 @@
 #!/bin/sh
 
-NR_ND=`awk '/^\*NODE$/{print NR}' $1`
-NR_SS=`awk '/^\*/{print NR}' $1 | grep -A1 ^$NR_ND$ | grep -v ^$NR_ND$`
+GLOBAL=0
+HORSE=0
+LOCAL=0
 
-cat $1 | \
-awk '{
-  if (NR > '$NR_ND' && NR < '$NR_SS'){
-    if($4==0) system("./hhorse.pl "sprintf("%d %f %f %f %f",$1,$2,$3,$4,6))
-    else print$0
-  }
-  else print$0
-}'
+if [ $# -lt 3 ] || [ ! -f $1 ]; then
+  echo "usage:\t`basename $0` file w0max -[ghl]" 1>&2
+else
+  DYNFILE=$1
+  w0max=$2
+  while [ $# -gt 2 ]; do
+    if [ $3 = "-g" ]; then
+      GLOBAL=1
+    fi
+    if [ $3 = "-h" ]; then
+      HORSE=1
+    fi
+    if [ $3 = "-l" ]; then
+      LOCAL=1
+    fi
+    shift
+  done
+
+  ND_NODE=`awk '/^\*NODE$/{print NR}' $DYNFILE`
+  NR_NEXT=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 ^$ND_NODE$ | grep -v ^$ND_NODE$`
+  DIRNAME=`dirname $0`
+
+  if [ $HORSE -ne 0 ]; then
+    cat $DYNFILE | \
+      awk '{
+        if (NR > '$ND_NODE' && NR < '$NR_NEXT' && $4==0){
+          system("'$DIRNAME'/hhorse.pl "sprintf("%d %f %f %f %f",$1,$2,$3,$4,'$w0max'))
+        }
+        else print$0
+      }'
+  fi
+fi
 
