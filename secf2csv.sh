98bcec00ad2febb5571a4055051ba80509c7a4fb
hakehash <b7dedc8797ec8ty@gmail.com>
2019-09-18 12:26:37 +0900

    stable release

--- secf2csv.sh
+++ secf2csv.sh
@@ -3,6 +3,7 @@
 if [ $# -eq 0 ]; then
   echo "usage:\tfind ./path/to/secforc | xargs -L1 `basename $0`" 1>&2
 else
+  LENGTH=9480
   while [ $# -ne 0 ]; do
     if [ -f $1 ]; then
       SECFORC=$1
@@ -13,11 +14,6 @@ else
   done
   ORIG=`cat $SECFORC | grep line -n | tail -n1 | sed 's/:.*//g'`
   AREA=`cat $SECFORC | awk 'NR=='$ORIG'+3{print $4}'`
-  if [ $# -eq 2 ]; then
-    LENGTH=$2
-  else
-    LENGTH=9480
-  fi
-  cat $SECFORC | awk '{if((NR-'$ORIG')%4==1) print $2/'$LENGTH' "," $3/'$AREA'}' > ${1}.csv
+  cat $SECFORC | awk '{if((NR-'$ORIG')%4==1) print $2/'$LENGTH' "," $3/'$AREA'}' > ${SECFORC}.csv
 fi
 
