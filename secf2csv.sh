23d0664a3529a944da37779cc906720f24fe10a5
hakehash <b7dedc8797ec8ty@gmail.com>
2019-09-18 14:23:25 +0900

    supported multiple sections

--- secf2csv.sh
+++ secf2csv.sh
@@ -3,7 +3,7 @@
 if [ $# -eq 0 ]; then
   echo "usage:\tfind ./path/to/secforc | xargs -L1 `basename $0`" 1>&2
 else
-  LENGTH=9480
+  LENGTH=6320
   while [ $# -ne 0 ]; do
     if [ -f $1 ]; then
       SECFORC=$1
@@ -14,6 +14,6 @@ else
   done
   ORIG=`cat $SECFORC | grep line -n | tail -n1 | sed 's/:.*//g'`
   AREA=`cat $SECFORC | awk 'NR=='$ORIG'+3{print $4}'`
-  cat $SECFORC | awk '{if((NR-'$ORIG')%4==1) print $2/'$LENGTH' "," $3/'$AREA'}' > ${SECFORC}.csv
+  cat $SECFORC | awk '(NR-'$ORIG')%4==1{if($1==1) print $2/'$LENGTH' "," $3/'$AREA'}' > ${SECFORC}.csv
 fi
 
