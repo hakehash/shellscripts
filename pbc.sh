d2191488ac522763ec82840b621926ee5a50c8aa
hakehash <b7dedc8797ec8ty@gmail.com>
2019-11-20 13:03:24 +0900

    minor fix

--- pbc.sh
+++ pbc.sh
@@ -5,19 +5,23 @@ NR_NODE=`awk '/^\*NODE$/{print NR}' $DYNFILE`
 NR_NEXT=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 ^${NR_NODE}$ | grep -v ^${NR_NODE}$`
 PATH_TO_SCRIPTS=`dirname $0`
 
-LENGTH_MAX=`cat $DYNFILE | awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' | grep -v \\$ | sort -n -k 2 | head -n1 | awk '{print $2}'`
+getxmin(){
+  cat $DYNFILE |\
+    awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' |\
+    grep -v \\$ | sort -n -k 2 | head -n1 | awk '{print $2}'
+}
 
-cat $DYNFILE |\
-  awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' |\
-  grep -v \\$ | sort -n -k 2 | head -n1 | awk '{print $2}'
+getxmax(){
+  cat $DYNFILE |\
+    awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' |\
+    grep -v \\$ | sort -n -k 2 | tail -n1 | awk '{print $2}'
+}
 
-LENGTH_MIN=`cat $DYNFILE | awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' | grep -v \\$ | sort -n -k 2 | tail -n1 | awk '{print $2}'`
+X_MIN=`getxmin`
+X_MAX=`getxmax`
 
-cat $DYNFILE |\
-  awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' |\
-  grep -v \\$ | sort -n -k 2 | tail -n1 | awk '{print $2}'
+echo $X_MIN
+echo $X_MAX
 
-echo $LENGTH_MAX
-echo $LENGTH_MIN
+#cat $DYNFILE | awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' | grep -v \\$ | sort -n -k 2
 
-cat $DYNFILE | awk '{if(NR > '$NR_NODE' && NR < '$NR_NEXT'){ print $0 }}' | grep -v \\$ | sort -n -k 2
