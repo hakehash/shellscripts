c7a248531f94d4a27d2ab99c0d4bf7557d4cf855
hakehash <b7dedc8797ec8ty@gmail.com>
2019-11-21 13:56:10 +0900

    minor change

--- pbc.sh
+++ pbc.sh
@@ -27,10 +27,17 @@ cat $DYNFILE |\
   '$2==xmin { print $1 > nodes_id_list_min.tmp }
    $2==xmax { print $1 > nodes_id_list_max.tmp }'
 
+DOF=1
+cat nodes_id_list_min.tmp |\
+  awk '{
+  printf("*CONSTRAINED_LINEAR_GLOBAL\n%10d\n%10d%10d%10.1f\n%10d%10d%10.1f\n",
+  '$DOF'*1000+NR, $1, '$DOF', -1, 900004, '$DOF', 1)}'
+
 for DOF in 2 3 5 6
 do
 paste nodes_id_list_min.tmp nodes_id_list_max.tmp |\
   awk '{
   printf("*CONSTRAINED_LINEAR_GLOBAL\n%10d\n%10d%10d%10.1f\n%10d%10d%10.1f\n",
-  '$DOF'*1000+NR,$1,'$DOF',-1,$2,'$DOF',1)}'
+  '$DOF'*1000+NR, $1, '$DOF', -1, $2, '$DOF', 1)}'
 done
+
