28d31615c1b07248fef559c9c6ef7d35c2e7a9e0
hakehash <b7dedc8797ec8ty@gmail.com>
2019-11-26 20:15:01 +0900

    debugging

--- line_notify.sh
+++ line_notify.sh
@@ -2,6 +2,7 @@
 
 URL="https://maker.ifttt.com/trigger"
 EVENTNAME="Analysis_Completed"
+#EVENTNAME="sh_tweet"
 YOUR_KEY="mPIF3JGH3SZIFUjSmDZovFmzdg3RzvA4Q6OHQ2d2kEm"
 
 WEBHOOKSURL="${URL}/${EVENTNAME}/with/key/${YOUR_KEY}"
@@ -9,6 +10,7 @@ WEBHOOKSURL="${URL}/${EVENTNAME}/with/key/${YOUR_KEY}"
 VALUE=`cat -`
 
 curl -X POST -H "Content-Type: application/json" -d \
+  #'{"EventName":"'$VALUE'"}' ${WEBHOOKSURL}
   '{"value1":"'$VALUE'"}' ${WEBHOOKSURL}
 echo
 exit 0
