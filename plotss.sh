85c6319c295cf65b11e2b3c18d168a8254d13299
hakehash <b7dedc8797ec8ty@gmail.com>
2019-12-12 14:35:26 +0900

    Englishnized

--- plotss.sh
+++ plotss.sh
@@ -3,7 +3,7 @@
 gnuplot -p -e 'set terminal pngcairo;
   set output "'`dirname $1`'/secforc.png";
   set monochrome; set xtics 0.001; unset key;
-  set ylabel "応力 [MPa]";
-  set xlabel "ひずみ [-]";
+  set ylabel "Stress [MPa]";
+  set xlabel "Strain [-]";
   set datafile separator ","; plot "'$1'" w l'
 
