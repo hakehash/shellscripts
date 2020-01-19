4c084b402e4537bf5d76adda5a087cfd7a395031
hakehash <b7dedc8797ec8ty@gmail.com>
2020-01-19 20:07:18 +0900

    initial commit

---
+++ resapp.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+if [ $# -lt 2 ] || [ ! -f $1 ]; then
+  echo "usage:\t`basename $0` file alpha" 1>&2
+else
+  a=3160
+  b=880
+  t=24
+  DYNFILE=$1
+  ALPHA=$2
+  shift 2
+  NR_NODE=`awk '/^\*NODE$/{print NR}' $DYNFILE`
+  NR_NEXT=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 "^${NR_NODE}$" | grep -v "^${NR_NODE}$"`
+  PATH_TO_SCRIPTS=`dirname $0`
+
+  cat $DYNFILE |\
+    awk 'BEGIN{
+        a='$a';
+        b='$b';
+        t='$t';
+        etat=33;
+        mesh_size=55;
+      }
+      function abs(x){
+        return x<0 ? -x:x;
+      }
+      {if(NR>'$NR_NODE' && NR<'$NR_NEXT' && $4==0){
+        if(int(abs($3-mesh_size)+0.5)%b==0){
+          y=$3-(mesh_size-etat);
+          printf("%8d%16G%16G%16G%8d%8d\n",$1,$2,y,$4,$5,$6);
+        } else if(int(abs($3+mesh_size)+0.5)%b==0){
+          y=$3+(mesh_size-etat);
+          printf("%8d%16G%16G%16G%8d%8d\n",$1,$2,y,$4,$5,$6);
+        } else {
+          print $0;
+        }
+      } else {
+        print $0;
+      }}'
+fi
+
