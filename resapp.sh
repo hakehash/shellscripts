de517ea625359e78c94d38f0a867e5438ffb6c44
hakehash <b7dedc8797ec8ty@gmail.com>
2020-01-19 23:59:45 +0900

    minor change

--- resapp.sh
+++ resapp.sh
@@ -11,6 +11,7 @@ else
   shift 2
   NR_NODE=`awk '/^\*NODE$/{print NR}' $DYNFILE`
   NR_NEXT=`awk '/^\*/{print NR}' $DYNFILE | grep -A1 "^${NR_NODE}$" | grep -v "^${NR_NODE}$"`
+  NR_END=`cat $DYNFILE | wc -l`
   PATH_TO_SCRIPTS=`dirname $0`
 
   cat $DYNFILE |\
@@ -20,22 +21,28 @@ else
         t='$t';
         etat=33;
         mesh_size=55;
+        NR_NEXT='$NR_END'
       }
       function abs(x){
         return x<0 ? -x:x;
       }
+      #/^\*NODE$/{NR_NODE=NR}
+      #NR>NR_NODE && NR<NR_NEXT && /^\*/{NR_NEXT=NR; print NR}
+
       {if(NR>'$NR_NODE' && NR<'$NR_NEXT' && $4==0){
         if(int(abs($3-mesh_size)+0.5)%b==0){
           y=$3-(mesh_size-etat);
           printf("%8d%16G%16G%16G%8d%8d\n",$1,$2,y,$4,$5,$6);
+        } else if(int(abs($3)+0.5)%b==0){
+          print $0
         } else if(int(abs($3+mesh_size)+0.5)%b==0){
           y=$3+(mesh_size-etat);
           printf("%8d%16G%16G%16G%8d%8d\n",$1,$2,y,$4,$5,$6);
-        } else {
+        } else if(int(abs($3)+0.5)%(b/2)==0){
           print $0;
         }
       } else {
-        print $0;
+        #print $0;
       }}'
 fi
 
